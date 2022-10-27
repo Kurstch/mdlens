852c348453c7df96cb482cce891b8c27264478cf
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-10-27 19:52:34 +0300

    Auto reload/reanalyze file on write

--- lua/mdlens/init.lua
+++ lua/mdlens/init.lua
@@ -1,4 +1,6 @@
+local util      = require("mdlens.util")
 local state     = require("mdlens.state")
+local File      = require("mdlens.File")
 local Workspace = require("mdlens.Workspace")
 
 local M = {}
@@ -9,6 +11,15 @@ function M.setup(config)
             state._workspaces[path] = Workspace:analyze(path)
         end
     end
+end
+
+function M.reload_current_file()
+    local filepath        = vim.api.nvim_buf_get_name(0)
+    local worksapce, file = util.find_file_from_workspaces(filepath)
+
+    if file == nil then return end
+
+    state._workspaces[worksapce.path].files[file.filepath] = File:analyze(file.filepath)
 end
 
 return M
--- plugin/mdlens.vim
+++ plugin/mdlens.vim
@@ -1,4 +1,5 @@
 highlight default link MDLens Comment
 
+autocmd BufWritePost *.md lua require("mdlens").reload_current_file()
 autocmd CursorMoved  *.md lua require("mdlens.hint").hint()
 autocmd CursorMovedI *.md lua require("mdlens.hint").hint()
